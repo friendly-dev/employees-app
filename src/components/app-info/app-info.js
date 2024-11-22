cb7624bfff4cd6e6299250e8233984e7e1cea46d
PC <you@example.com>
2024-11-23 02:09:36 +0300

    Initial commit

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,14 @@
+import './app-info.css';
+
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Учет сотрудников компании №</h1>
+            <h2>Общее число сотрудников</h2>
+            <h3>Премию получат:</h3>
+        </div>
+    )
+}
+
+export default AppInfo;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './components/app/app.js';
+// import AppInfo from './components/app-info/app-info.js';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+    {/* <AppInfo /> */}
+  </React.StrictMode>
+);
+
+
